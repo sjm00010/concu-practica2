d058829b71f0af3e218fbdc91a575ee822dd290d
Sergio <sjm00010@red.ujaen.es>
2019-04-07 00:30:18 +0200

    Monitor V1.0

--- src/jimenezmorenosergioprac2/MonitorMemoria.java
+++ src/jimenezmorenosergioprac2/MonitorMemoria.java
@@ -1,70 +1,140 @@
 package jimenezmorenosergioprac2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import jimenezmorenosergioprac2.JimenezMorenoSergioPrac2.Tipo;
 
 /**
  *
  * @author Sergio Jiménez Moreno
  */
 public class MonitorMemoria {
+    // Constantes
+    private final static int MAX_MARCOS = 4;
+    
     // Variables
     private int numMarcos;
     private Semaphore exmMonitor;
-    private List<RepresentaProceso> listaProcesos;
+    private HashMap<Integer,RepresentaProceso> listaProcesos;
+    private List<Peticion> listaPetiones;
+    private List<Peticion> listaPetionesLiberacion;
     
     // Funciones publicas
-    public boolean compruebaPagina(int idProceso, int pagina){
-        boolean resultado = false;
-        for (RepresentaProceso proceso : listaProcesos) {
-            if(proceso.getIdProceso() == idProceso){
+    public boolean compruebaPagina(int idProceso, int pagina) throws InterruptedException{
+        exmMonitor.acquire();
+        try{
+            boolean resultado = false;
+            RepresentaProceso proceso = listaProcesos.get(idProceso);
+            if(proceso != null){
                 resultado = proceso.compruebaPagina(pagina);
             }
+            return resultado;
+        }finally{
+            exmMonitor.release();
         }
-        return resultado;
+        
     }
     
+    public void addPeticion(Peticion peticion) throws InterruptedException{
+        exmMonitor.acquire();
+        try {
+            listaPetiones.add(peticion);
+        }finally{
+            exmMonitor.release();
+        }
+    }
     
+    public void addPeticionLiberacion(Peticion peticion) throws InterruptedException{
+        exmMonitor.acquire();
+        try {
+            listaPetionesLiberacion.add(peticion);
+        }finally{
+            exmMonitor.release();
+        }
+    }
+    
+    public void atenderPeticionLiberacion() throws InterruptedException{
+        exmMonitor.acquire();
+        try {
+            while(!listaPetionesLiberacion.isEmpty()){
+                liberarMarcos(listaPetionesLiberacion.get(0));
+                listaPetionesLiberacion.remove(0);
+            }
+        }finally{
+            exmMonitor.release();
+        }
+    }
+    
+    public void atenderPeticion() throws InterruptedException{
+        exmMonitor.acquire();
+        try{
+            if (numMarcos > 1) {
+                Peticion peticion = listaPetiones.remove(0);
+                if (peticion.getTipo() == Tipo.CARGA) {
+                    asignarMarcosInicio(peticion.getIdProceso());
+                } else {
+                    falloPagina(peticion.getIdProceso(), peticion.getPagina());
+                }
+            } else {
+                Peticion peticion = removeFallo();
+                falloPagina(peticion.getIdProceso(), peticion.getPagina());
+            }
+        }finally{
+            exmMonitor.release();
+        }
+    }
     
     // Funciones auxiliares
     private void asignarMarcosInicio(int idProceso){
         numMarcos -= 2;
         RepresentaProceso nuevoProceso = new RepresentaProceso(idProceso);
-        listaProcesos.add(nuevoProceso);
+        listaProcesos.put(idProceso, nuevoProceso);
     }
     
-    private void asignarMarcos(int idProceso, int pagina){
-        numMarcos--;
-        for (RepresentaProceso proceso : listaProcesos) {
-            if(proceso.getIdProceso() == idProceso){
+    private boolean asignarMarcos(int idProceso, int pagina){
+        boolean resultado = false;
+        RepresentaProceso proceso = listaProcesos.get(idProceso);
+        if(proceso != null){
+            if (proceso.paginasAsignadas() < MAX_MARCOS ){
+                numMarcos--;
                 proceso.addPagina(pagina);
-                //finalizar bucle, sin break T.T
+                resultado = true;
             }
         }
+        return resultado;
     }
     
     private void falloPagina(int idProceso, int pagina){
-        if( numMarcos > 0){
-            asignarMarcos(idProceso, pagina);
-        }else{
-            for (RepresentaProceso proceso : listaProcesos) {
-                if(proceso.getIdProceso() == idProceso){
-                    proceso.sustituyePagina(pagina);
-                    //finalizar bucle, sin break T.T
-                }
+        boolean asignado = false;
+        
+        if( numMarcos > 0 ){
+            asignado = asignarMarcos(idProceso, pagina);
+        }
+        
+        if(!asignado){
+            RepresentaProceso proceso = listaProcesos.get(idProceso);
+            if(proceso != null){
+                proceso.sustituyePagina(pagina);
             }
         }
     }
     
     private void liberarMarcos(Peticion peticion){
-        RepresentaProceso encontrado = null;
-        for (RepresentaProceso proceso : listaProcesos) {
-            if(proceso.getIdProceso() == peticion.getIdProceso()){
-                numMarcos += proceso.paginasAsignadas();
-                encontrado = proceso;
-                //finalizar bucle, sin break T.T
+        RepresentaProceso proceso = listaProcesos.remove(peticion.getIdProceso());
+        if(proceso != null){
+            numMarcos += proceso.paginasAsignadas();
+        }
+    }
+    
+    private Peticion removeFallo(){
+        Peticion resultado = null;
+        for (int i = 0; i < listaPetiones.size(); i++) {
+            if(listaPetiones.get(i).getTipo() == Tipo.FALLO){
+                resultado = listaPetiones.remove(i);
+                i = listaPetiones.size();
             }
         }
-        listaProcesos.remove(encontrado);
+        return resultado;
     }
 }
